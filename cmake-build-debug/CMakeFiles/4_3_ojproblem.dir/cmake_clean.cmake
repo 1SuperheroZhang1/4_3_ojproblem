2e09eb5520476935f2f7702f6dfdb98caca81c0a
zhangcy <2304417358@qq.com>
2023-01-08 18:37:24 +0800

    C语言督学营 - 4.3 课时作业4

---
+++ cmake-build-debug/CMakeFiles/4_3_ojproblem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "4_3_ojproblem.exe"
+  "4_3_ojproblem.exe.manifest"
+  "4_3_ojproblem.pdb"
+  "CMakeFiles/4_3_ojproblem.dir/main.c.obj"
+  "lib4_3_ojproblem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/4_3_ojproblem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
